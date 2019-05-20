626e8f545bc0a04b566ed18eaa5eaa0b8e9ead96
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-05-20 18:29:19 -0300

    Chamando a API e listando

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
 const carregando = () => {
-  console.log("HELLO"); 
+  $.getJSON("https://dog.ceo/api/breeds/list/all", function(data){
+    let lista = (Object.keys(data.message));
+    lista.forEach(element => {
+      $("#racas").before(`<option value="${element}">${element}</option>`);
+    });    
+  });
+}
+const lista = (raca) => {
+  $.getJSON("https://dog.ceo/api/breeds/list/all", function(data){
+    let lista = (data.message[raca]);
+    if (lista.length>0) {
+      $("#formSubRaca").show();
+      lista.forEach(element => {
+        $("#subraca").before(`<option value="${element}">${element}</option>`)
+      })
+    }else{
+      $("#formSubRaca").hide();
+    }
+    imgBanner(raca)
+  })
+}
+const sublista = (subRaca) => {
+  let raca = $("#formRaca").val();
+  raca += `/${subRaca}`;
+  imgBanner(raca)
+}
+
+const numRandom = (max) => {
+  return (Math.random()*(max))
+}
+
+const imgBanner = (raca) => {
+  $.getJSON(`https://dog.ceo/api/breed/${raca}/images`, function(imagem){
+    let index = Math.trunc(numRandom(imagem.message.length));      
+    let novaImg = imagem.message[index];
+    $("#imgDog").attr("src",novaImg);
+  })
 }
